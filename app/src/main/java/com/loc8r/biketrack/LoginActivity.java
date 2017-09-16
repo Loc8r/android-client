c6f897cdf28cf6b887549a1fec87cb4cbbe06bcc
Alex White <a24white@edu.uwaterloo.ca>
2017-09-16 18:10:50 -0400

    Fix indentation

--- app/src/main/java/com/loc8r/biketrack/LoginActivity.java
+++ app/src/main/java/com/loc8r/biketrack/LoginActivity.java
@@ -33,139 +33,138 @@ import java.util.Iterator;
 
 public class LoginActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
-    GoogleApiClient mgac;
-    GoogleSignInAccount mgsa;
-    DatabaseReference mDatabaseReference;
-    DataSnapshot mUser;
+	FirebaseAuth mAuth;
+	GoogleApiClient mgac;
+	GoogleSignInAccount mgsa;
+	DatabaseReference mDatabaseReference;
+	DataSnapshot mUser;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-        //TODO potential error with logging to google account
+		//TODO potential error with logging to google account
 
-        FirebaseApp.initializeApp(this);
-        mAuth = FirebaseAuth.getInstance();
+		FirebaseApp.initializeApp(this);
+		mAuth = FirebaseAuth.getInstance();
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .requestIdToken(getString(R.string.server_client_id))
-                .build();
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+				.requestEmail()
+				.requestIdToken(getString(R.string.server_client_id))
+				.build();
 
-        mgac = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        Toast.makeText(LoginActivity.this, "Oops! Something went wrong. Please restart program", Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                })
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+		mgac = new GoogleApiClient.Builder(this)
+				.enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+					@Override
+					public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+						Toast.makeText(LoginActivity.this, "Oops! Something went wrong. Please restart program", Toast.LENGTH_SHORT)
+								.show();
+					}
+				})
+				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+				.build();
 
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
-    }
+		mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+	}
 
-    public void loginWithGoogle(View v) {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mgac);
-        startActivityForResult(signInIntent, 1);
-    }
+	public void loginWithGoogle(View v) {
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mgac);
+		startActivityForResult(signInIntent, 1);
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //TODO figure out why signin result fails!
-        GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-        handleSignInResult(result);
-    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		//TODO figure out why signin result fails!
+		GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+		handleSignInResult(result);
+	}
 
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            mgsa = result.getSignInAccount();
-            authenticateFirebaseUser(mgsa);
-        } else {
-            Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
-        }
-    }
+	private void handleSignInResult(GoogleSignInResult result) {
+		if (result.isSuccess()) {
+			mgsa = result.getSignInAccount();
+			authenticateFirebaseUser(mgsa);
+		} else {
+			Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+		}
+	}
 
-    private void authenticateFirebaseUser(GoogleSignInAccount account) {
+	private void authenticateFirebaseUser(GoogleSignInAccount account) {
 
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
+		AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+		mAuth.signInWithCredential(credential)
+				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> task) {
+						if (task.isSuccessful()) {
 
-                            //TODO get firebase data if user is in system
-                            getFirebaseData(mDatabaseReference);
+							//TODO get firebase data if user is in system
+							getFirebaseData(mDatabaseReference);
 
-                        } else {
-                           //TODO if no account set up then tell person to add an account
-                            Toast.makeText(LoginActivity.this, "Sorry! Something went wrong! Try again!",
-                                    Toast.LENGTH_SHORT).show();
-                        }
+						} else {
+							//TODO if no account set up then tell person to add an account
+							Toast.makeText(LoginActivity.this, "Sorry! Something went wrong! Try again!",
+									Toast.LENGTH_SHORT).show();
+						}
 
-                    }
-                });
-    }
+					}
+				});
+	}
 
-    private boolean userExists(DataSnapshot snapshot) {
+	private boolean userExists(DataSnapshot snapshot) {
 
+		FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+		String checkedEmail = user.getEmail();
+		Iterator<DataSnapshot> it = snapshot.getChildren().iterator();
+		while (it.hasNext()) {
+			DataSnapshot child = it.next();
+			//TODO fix line below.
+			User newUser = (User) child.getValue();
+			String inputEmail = newUser.getAcctEmail();
 
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            String checkedEmail = user.getEmail();
-            Iterator<DataSnapshot> it = snapshot.getChildren().iterator();
-            while (it.hasNext()) {
-                DataSnapshot child = it.next();
-                //TODO fix line below.
-                User newUser = (User) child.getValue();
-                String inputEmail = newUser.getAcctEmail();
+			if (checkedEmail.equals(inputEmail)) {
+				mUser = child;
+				return true;
+			}
 
-                if (checkedEmail.equals(inputEmail)) {
-                    mUser = child;
-                    return true;
-                }
+			return false;
+		}
 
-            return false;
-            }
+		return false;
+	}
 
-        return false;
-    }
+	private void getFirebaseData(DatabaseReference ref) {
+		ref.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				//TODO get the location of the specific user and package it as an intent to MapsActivity
 
-    private void getFirebaseData(DatabaseReference ref) {
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                //TODO get the location of the specific user and package it as an intent to MapsActivity
+				if (userExists(dataSnapshot)) {
+					LatLng location = (LatLng) mUser.child("latlng").getValue();
+					goToMaps(location);
+				} else {
+					//TODO make person register the bike SIM with firebase
+					Intent intent = new Intent(LoginActivity.this, SignUpUser.class);
+					startActivity(intent);
+				}
 
-                if (userExists(dataSnapshot)) {
-                    LatLng location = (LatLng) mUser.child("latlng").getValue();
-                    goToMaps(location);
-                } else {
-                    //TODO make person register the bike SIM with firebase
-                    Intent intent = new Intent(LoginActivity.this, SignUpUser.class);
-                    startActivity(intent);
-                }
+			}
 
-            }
+			@Override
+			public void onCancelled(DatabaseError databaseError) {
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+			}
+		});
+	}
 
-            }
-        });
-    }
+	private void goToMaps(LatLng location) {
 
-    private void goToMaps(LatLng location) {
-
-        Intent intent = new Intent(this, MapsActivity.class);
-        double lat = location.latitude;
-        double lon = location.longitude;
-        intent.putExtra("latitude", lat);
-        intent.putExtra("longitude", lon);
-        startActivity(intent);
-    }
+		Intent intent = new Intent(this, MapsActivity.class);
+		double lat = location.latitude;
+		double lon = location.longitude;
+		intent.putExtra("latitude", lat);
+		intent.putExtra("longitude", lon);
+		startActivity(intent);
+	}
 }

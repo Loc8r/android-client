4bcb24569e90fb6adb214e562c57e029c1b26653
Mickey Dang <mickeydang7299@gmail.com>
2017-09-16 11:25:31 -0400

    More Progress

--- app/src/main/java/com/loc8r/biketrack/LoginActivity.java
+++ app/src/main/java/com/loc8r/biketrack/LoginActivity.java
@@ -17,9 +17,12 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -49,25 +52,20 @@ public class LoginActivity extends AppCompatActivity {
                 .build();
 
 
-
-        mAuth = FirebaseAuth.getInstance();
+//        FirebaseApp.initializeApp(this);
+//        mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
-
-    private boolean checkPassword() {
-        return true;
-    }
-
 
     public void loginWithGoogle(View v) {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mgac);
         startActivityForResult(signInIntent, 1);
-
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //TODO figure out why signin result fails!
         GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
         handleSignInResult(result);
     }
@@ -75,14 +73,41 @@ public class LoginActivity extends AppCompatActivity {
     private void handleSignInResult(GoogleSignInResult result) {
         if (result.isSuccess()) {
             mgsa = result.getSignInAccount();
+           authenticateFirebaseUser(mgsa);
         } else {
             Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void createFirebaseUser() {
+    private void authenticateFirebaseUser(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMaps();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void getFirebaseData() {
 
     }
 
-
+    private void goToMaps() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }

f6269aca858207609ff4f51efd69e37aebca83c9
Mickey Dang <mickeydang7299@gmail.com>
2017-09-16 03:48:17 -0400

    More Progress

--- app/src/main/java/com/loc8r/biketrack/LoginActivity.java
+++ app/src/main/java/com/loc8r/biketrack/LoginActivity.java
@@ -1,26 +1,88 @@
 package com.loc8r.biketrack;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    GoogleApiClient mgac;
+    GoogleSignInAccount mgsa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+       mgac = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(LoginActivity.this, "Error! Login failed! Please Try Again!", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private boolean checkPassword() {
         return true;
     }
 
+
     public void loginWithGoogle(View v) {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mgac);
+        startActivityForResult(signInIntent, 1);
+
 
     }
 
-    public void loginWithEmail(View v) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        handleSignInResult(result);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            mgsa = result.getSignInAccount();
+        } else {
+            Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createFirebaseUser() {
 
     }
+
+
 }
